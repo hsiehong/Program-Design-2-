35cf76dc1bd449e9d96a0803d225b9d384cab5da
hsiehong <hsiehong1022@gmail.com>
2018-04-21 02:31:27 +0800

    P2W2 treasure finish

--- P2W2/treasure_many/treasure_many.cpp
+++ P2W2/treasure_many/treasure_many.cpp
@@ -1,40 +1,40 @@
 #include<iostream>
-#include<vector>
 #include<queue>
-#define N 5000
+#include<vector>
+#define N 50000
 #define M 100000
 
 using namespace std;
 
 int main(void){
-    int n,m,t,u,v,k;
-    int visit[N],start[N],c[N];
-    vector <int> edge[M];
-    queue <int> que;
+    int n,m,t,start[N],c[N],u,v,k,mymax;
+    bool flag[N];
     while(1){
-       cin>>n>>m>>t;if(n==0)break;
+        cin>>n>>m>>t;if(n==0)break;
+        vector <int> edge[M];// the neighbor of each city
+        queue <int> que;// we need to go through all cities to find the maxtreasure
         for(int i=0;i<t;i++)cin>>start[i];
         for(int i=0;i<n;i++)cin>>c[i];
         for(int i=0;i<m;i++){
             cin>>u>>v;
             edge[u].push_back(v);
         }
-        for(int i=0;i<n;i++)visit[i]=0;
-        int mymax=0;
-            //breadth first search
+        mymax=0;
+        for(int i=0;i<n;i++)flag[i]=false;
         for(int i=0;i<t;i++){
             k=start[i];
-            //if(visit[k])continue;
-            visit[k]=1;
-            que.push(k);
+            if(flag[k])continue; //若k已被拜訪過，則表示k可到的地方也已被拜訪過
+            flag[k]=true;
+            que.push(k); //k is the first city
             while(!que.empty()){
                 u=que.front();
                 que.pop();
-                mymax=(c[u]>mymax)?c[u]:mymax;
-                for(auto &v:edge[u]){   //for each neighbor of u
-                    if(visit[v]==0){
+                if(c[u]>mymax)mymax=c[u];
+                    //若vector沒有重新宣告，有可能會跑到上一筆的資料(auto會自己跑完)
+                for(auto &v:edge[u]){ // 將所有k可到的城鎮加入que中
+                    if(!flag[v]){
                         que.push(v);
-                        visit[v]=1;
+                        flag[v]=true;
                     }
                 }
             }
